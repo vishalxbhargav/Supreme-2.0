e2ca701a42ceec9e8dc89616daf88a9d7e4cc795
vishalxbhargav <vishalxbhargav@gmail.com>
2024-01-02 22:05:24 +0530

    stack implimentation using class

--- Stack/ImplimentUsingClass.cpp
+++ Stack/ImplimentUsingClass.cpp
@@ -1,5 +1,37 @@
 #include<iostream>
 using namespace std;
 class Stack{
+    int *arr;
+    int top;
+    int size;
     public:
+    Stack(int n){
+        arr=new int [n];
+        this->size=size;
+        this->top=-1;
+    }
+    void push(int n){
+        if(top==size-1){
+            cout<<"Stack Overflow ";
+            return;
+        }
+        arr[++top]=n;
+        return;
+    }
+    void pop(){
+        if(top==-1){
+            cout<<"Stack Underflow";
+            return;
+        }
+        top--;
+        return;
+    }
+    bool isEmpty(){
+        if(top==-1) return true;
+        return false;
+    }
+    int getTop(){
+        if(isEmpty()) cout<<"Stack isEmpty";
+        return arr[top];
+    }
 };
